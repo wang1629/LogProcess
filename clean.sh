83094d7b2b0e689fdccf69b6947a1c5f24752d9f
wang <wang@centos_t.(none)>
2014-09-06 05:34:05 -0700

    add clean.sh

---
+++ clean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "" > fakeServer/res/master.log
+echo "" > fakeServer/res/sumWorker.log
+echo "" > fakeServer/res/sqrtWorker.log
+
+echo "" > processChain/class/result_file.txt
+rm processChain/class/*.class
